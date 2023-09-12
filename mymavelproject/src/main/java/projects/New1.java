7cc54d4dd7da6f2804113df4e888562646bf27f0
Dell <Dell@DESKTOP-SETA581>
2023-09-12 12:31:15 +0530

    My first commit

---
+++ mymavelproject/src/main/java/projects/New1.java
@@ -0,0 +1,15 @@
+package projects; //static method
+
+public class New
+{
+public static void show()
+{
+	System.out.println("Gud mrng");
+}
+	public static void main(String args[])
+	{
+	
+		show();
+	
+}
+}
